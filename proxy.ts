b868e0a890f444f6292a90e3d634b59006190abd
vyg.re <241956544+m-health-dev@users.noreply.github.com>
2025-11-06 19:09:32 +0700

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,20 +1,10 @@
 import createMiddleware from "next-intl/middleware";
-import { NextResponse } from "next/server";
 import { routing } from "./i18n/routing";
 
-export default function middleware(req: Request) {
-  const url = new URL(req.url);
-
-  // Jika environment = production → redirect semua ke /coming-soon
-  if (process.env.NODE_ENV === "development") {
-    // Kecualikan path yang sudah di /coming-soon supaya tidak loop redirect
-    if (!url.pathname.startsWith("/coming-soon")) {
-      url.pathname = "/coming-soon";
-      return NextResponse.redirect(url);
-    }
-  }
-}
-
+export default createMiddleware(routing);
 export const config = {
+  // Match all pathnames except for
+  // // - … if they start with /api, /trpc, /_next or /_vercel
+  // // - … the ones containing a dot (e.g. favicon.ico)
   matcher: "/((?!api|coming-soon|trpc|_next|_vercel|.*\\..*).*)",
 };
